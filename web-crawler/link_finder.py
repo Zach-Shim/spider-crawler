a9f2caf0c037b6ed569426839cba550336ae1467
Zach Shim <46553692+Zach-Shim@users.noreply.github.com>
2020-09-16 23:07:26 -0700

    Updated documentation
    fixed spacing and comments

--- web-crawler/link_finder.py
+++ web-crawler/link_finder.py
@@ -1,24 +1,23 @@
-# this class parses data from an html page
+# link_finder.py
+# class utilizes built in python import that parses data from an html page
 
 from html.parser import HTMLParser
 from urllib import parse
 
-# inhertied from HTMLParser
-# HTMLParser allows us to not make custom parser code
-class LinkFinder(HTMLParser):
+# class is inhertied from HTMLParser; HTMLParser allows us to not make custom parser code
+class LinkFinder(HTMLParser):                               
 
-    # overloaded method
-    # (constructor) initializes base url, page url, and makes links an empty set
+    # overloaded method (constructor); initializes base url, page url, and makes links an empty set
     def __init__(self, base_url, page_url):
         super().__init__()
         self.base_url = base_url
         self.page_url = page_url
         self.links = set()
     
-    # overloaded method
+   # when we call HTMLParser feed() this function is called when it encounters an opening tag <a>
     def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            for (attribute, value) in attrs:
+        if tag == 'a':                                                      
+            for (attribute, value) in attrs:                           
                 if attribute == 'href':
                     url = parse.urljoin(self.base_url, value)
                     self.links.add(url)
@@ -29,4 +28,3 @@ class LinkFinder(HTMLParser):
     # abstract method
     def error(self, message):
         pass
-
